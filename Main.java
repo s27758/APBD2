ac2dd2cbe61c8ac694140d6f7e2d4aa3147a085f
s27758 <s27758@pjwstk.edu.pl>
2024-03-14 10:05:51 +0100

    Modyfikacja 2

--- Main.java
+++ Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int number = 10;
+        for (int i = 0; i < number; i++) {
+        }
+
+    }
 }
