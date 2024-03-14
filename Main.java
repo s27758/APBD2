f22713ff706d602c3fac13ec207154960e453ec0
s27758 <s27758@pjwstk.edu.pl>
2024-03-14 10:27:28 +0100

    Dodano find-max

--- Main.java
+++ Main.java
@@ -7,6 +7,8 @@ public class Main {
         tab[0] = number;
         double average = Main.calculateAverage(tab);
         System.out.println("Average: " + average);
+        int max = Main.findMax(tab);
+        System.out.println("Maksymalna wartość w tablicy: " + max);
     }
     public static double calculateAverage(int[] numbers) {
         if (numbers == null || numbers.length == 0) {
@@ -17,5 +19,18 @@ public class Main {
             sum += number;
         }
         return (double) sum / numbers.length;
+    }
+    public static int findMax(int[] array) {
+        if (array == null || array.length == 0) {
+            return 0;
+        }
+
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
     }
 }
